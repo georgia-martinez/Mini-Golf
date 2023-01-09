dc1b195785fbd8fb9d8ba6d19e6ed69930079254
georgia-martinez <gcm49@case.edu>
2023-01-08 22:42:20 -0600

    Ball bounces if it hits the edge of the screen

--- ball.go
+++ ball.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"math"
-
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -20,13 +19,16 @@ type Ball struct {
 
 	canStartMoving bool
 	isMoving       bool
+
+	dirX float64
+	dirY float64
 }
 
 func (ball *Ball) SetPosition(x float64, y float64) {
 	ball.x = x
 	ball.y = y
 
-	var width, height = ball.img.Size()
+	width, height := ball.img.Size()
 
 	ball.centerX = ball.x + (float64(width) / 2)
 	ball.centerY = ball.y + (float64(height) / 2)
@@ -35,6 +37,9 @@ func (ball *Ball) SetPosition(x float64, y float64) {
 func (ball *Ball) SetInitialVelocity(angle float64, power float64) {
 	ball.vx0 = -math.Cos(angle) * power
 	ball.vy0 = math.Sin(angle) * power
+
+	ball.dirX = 1
+	ball.dirY = 1
 }
 
 func (ball *Ball) ResetVelocity() {
@@ -45,11 +50,33 @@ func (ball *Ball) ResetVelocity() {
 }
 
 func (ball *Ball) MovePosition() {
+
+	// Changing the ball's velocity
+	const DRAG = .9
+
+	ball.vx0 *= ball.dirX * DRAG
+	ball.vy0 *= ball.dirY * DRAG
+
+	// Make ball bounce if it hits a wall
+	width, height := ball.img.Size()
+	
+	w := float64(width) / 2
+	h := float64(height) / 2
+
+	if ball.centerX - w < 0 || ball.centerX + w > screenWidth {
+		ball.dirX *= -1
+		ball.x = GetClosestNumber(0, float64(screenWidth - width), ball.centerX)
+	}
+
+	if ball.centerY - h < 0 || ball.centerY + h > screenHeight {
+		ball.dirY *= -1
+		ball.y = GetClosestNumber(0, float64(screenHeight - height), ball.centerY)
+	}
+
+	// Changing the ball's position
 	ball.SetPosition(ball.x+ball.vx0, ball.y+ball.vy0)
 
-	ball.vx0 *= .9
-	ball.vy0 *= .9
-
+	// Reset the velocity is the ball is done moving
 	if math.Round(ball.vx0) == 0 && math.Round(ball.vy0) == 0 {
 		ball.ResetVelocity()
 	}
--- util.go
+++ util.go
@@ -7,3 +7,11 @@ import (
 func GetDistance(x0 float64, y0 float64, x1 float64, y1 float64) float64 {
 	return math.Sqrt(math.Pow(x1 - x0, 2) + math.Pow(y1 - y0, 2))
 }
+
+func GetClosestNumber(a float64, b float64, c float64) float64 {
+	if math.Abs(c - a) < math.Abs(c - b) {
+		return a
+	} else {
+        return b
+	}
+}
